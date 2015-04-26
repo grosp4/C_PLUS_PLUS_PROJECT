978edf9db7d6df05d84bfba3cf06b1ac950cb41e
bartj2 <joelbaer@hotmail.com>
2015-04-26 17:40:03 +0200

    added TextFile Class

---
+++ TextFile.h
@@ -0,0 +1,39 @@
+/** \file       Template.hpp
+ *  \brief
+ *******************************************************************************
+ *
+ *  \class
+ *
+ *  \brief
+ *
+ *  \authors
+ *
+ *  \date
+ *
+ *  \remark     Last Modification
+ *               \li
+ *
+ ******************************************************************************/
+/* Define to prevent recursive inclusion --------------------------------------*/
+#ifndef TEXTFILE_H
+#define TEXTFILE_H
+
+/* Imports Library */
+#include <string>
+
+/* Class Constant Declaration--------------------------------------------------*/
+
+/* Define Coordinate System for Left Team */
+
+/* Class Type declaration -----------------------------------------------------*/
+/* Class Data declaration -----------------------------------------------------*/
+/* Class definition -----------------------------------------------------------*/
+class TextFile
+{
+public:
+    TextFile();
+    ~TextFile();
+    void write();
+};
+
+#endif // TEXTFILE_H
